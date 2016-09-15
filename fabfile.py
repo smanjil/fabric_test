57187fbb20c7972cacfe24dd70a2c4b29c953070
Manjil Shrestha <manjil.shrestha@yipl.com.np>
2016-09-15 17:14:26 +0545

    added prepare_deploy() with git instructions

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,10 @@
 
+from fabric.api import local
+
 def hello(name='Fabric!'):
     print 'Hello {0}' .format(name)
+
+def prepare_deploy():
+    local('./manage.py test fabapp')
+    local('git add -p && git commit')
+    local('git push')
