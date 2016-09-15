fba3114a7e0792d522e0181be26f06d5142054bc
Manjil Shrestha <manjil.shrestha@yipl.com.np>
2016-09-15 17:28:22 +0545

    added helper text for all methods

--- fabfile.py
+++ fabfile.py
@@ -5,13 +5,15 @@ def hello(name='Fabric!'):
     print 'Hello {0}' .format(name)
 
 def test():
+    print 'App Testing....'
     local('./manage.py test fabapp')
 
 def commit():
+    print 'Committing the changes....'
     local('git add -p && git commit')
 
 def push():
-    print 'Deploying.....'
+    print 'Deploying the app....'
     local('git push')
 
 def prepare_deploy():
